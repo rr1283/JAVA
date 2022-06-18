9b222aa46ce8593f1bbbdfa67cc7c79d5c06620d
rr1283 <rr1283@mail.ru>
2022-06-18 16:56:56 +0300

    HW4 TestMethods3

---
+++ basejava/test/com/basejava/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,11 @@
+package com.basejava.webapp.storage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    private static final Storage storage = new ArrayStorage();
+
+    public ArrayStorageTest() {
+
+        super(storage);
+    }
+}
---
+++ basejava/test/com/basejava/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,11 @@
+package com.basejava.webapp.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    private static final Storage storage = new SortedArrayStorage();
+
+    public SortedArrayStorageTest() {
+        super(storage);
+    }
+
+}
